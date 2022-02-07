99daa17b6286daac18e212aced40298ddc91d11a
Jungwon Kim <jwkim@4dreplay.com>
2022-02-07 16:07:32 +0900

    fix: API 28 이하에서 SystemUiVisibility 설정 시 Edge To Edge 설정이 풀리는 버그 수정
    문서 상에는 두 메서드가 충돌나는 경우 View.SetSystemUiVisibility 사용을 discontinue 하라고 되어있음.

--- app/src/main/java/com/manorgass/togglefullscreentest/MainActivity.kt
+++ app/src/main/java/com/manorgass/togglefullscreentest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.manorgass.togglefullscreentest
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -7,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
+    private var isEdgeToEdge = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,21 +22,40 @@ class MainActivity : AppCompatActivity() {
     private fun initView() {
         findViewById<Button>(R.id.btnFullScreenOn).setOnClickListener { setFullScreen() }
         findViewById<Button>(R.id.btnFullScreenOff).setOnClickListener { initFullScreen() }
-        findViewById<Button>(R.id.btnEdgeToEdgeOn).setOnClickListener { setEdgeToEdge(false) }
-        findViewById<Button>(R.id.btnEdgeToEdgeOff).setOnClickListener { setEdgeToEdge(true) }
+        findViewById<Button>(R.id.btnEdgeToEdgeOn).setOnClickListener { enableEdgeToEdge() }
+        findViewById<Button>(R.id.btnEdgeToEdgeOff).setOnClickListener { disableEdgeToEdge() }
     }
 
     private fun setFullScreen() {
         // 사용자 제스쳐 시 System Bars가 나타났다 사라지는 정책 설정.
         window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        // API 28 이하부터 Edge To Edge 설정한 후 systemUiVisibility 변경 시 기존에 설정한 Edge To Edge 설정이 초기화 되는 현상을 막기위한 처리
+        if (Build.VERSION.SDK_INT < 29 && isEdgeToEdge) {
+            enableEdgeToEdge()
+        }
     }
 
     private fun initFullScreen() {
         // 0으로 설정 시 Clear 됨.
         window?.decorView?.systemUiVisibility = 0
+
+        // API 28 이하부터 Edge To Edge 설정한 후 systemUiVisibility 변경 시 기존에 설정한 Edge To Edge 설정이 초기화 되는 현상을 막기위한 처리
+        if (Build.VERSION.SDK_INT < 29 && isEdgeToEdge) {
+            enableEdgeToEdge()
+        }
+    }
+
+    private fun enableEdgeToEdge() {
+        setEdgeToEdge(false)
+    }
+
+    private fun disableEdgeToEdge() {
+        setEdgeToEdge(true)
     }
 
     private fun setEdgeToEdge(fitSystemWindow: Boolean) {
+        isEdgeToEdge = !fitSystemWindow
         WindowCompat.setDecorFitsSystemWindows(window, fitSystemWindow)
     }
 }
